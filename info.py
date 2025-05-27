6e10c2f6fb6bbd5f2d1225141242894a40f6bb1a
CHIAROxL <lorenzo.chiarilli.lc@gmail.com>
2025-05-27 17:47:25 +0200

    aggiornamento

--- info.py
+++ info.py
@@ -1,16 +1,47 @@
 def main():
     import pubchempy as pcp
 
-    tipo = input("1 - CID\n2 - Formula bruta\n3 - Nome IUPAC\n4 - Sinonimo\ncome vuoi cercare il composto? ")
+    tipo = input("1 - CID\n2 - Formula\n3 - Nome\ncome vuoi cercare il composto? ")
+    print("---")
+    tipo = tipo.lower()
 
-    tipo_1 = ["1", "CID", "Cid", "cid", "compound identifier",]
+    tipo_1 = ["1", "cid", "compound identifier"]
+    tipo_2 = ["2", "formula"]
+    tipo_3 = ["3", "nome"]
+
     if tipo in tipo_1:
         composto = pcp.Compound.from_cid(input("inserisci il CID del composto: "))
-        print("nome IUPAC: \"" + composto.iupac_name + "\"")
+        print("---")
+        print("nome: \"" + composto.iupac_name + "\"")
         print("ecco i primi tre sinonimi secondo PubChem: " + str(composto.synonyms[:3]))
         print("la formula del composto è " + composto.molecular_formula)
         print("il peso molecolare del composto è di " + composto.molecular_weight + " g/mol")
         print("il composto ha carica " + str(composto.charge))
+        print("---")
+
+    elif tipo in tipo_2:
+        composto = pcp.get_compounds(input("inserisci la formula: "), "formula")[0]
+        print("---")
+        print("nome: \"" + composto.iupac_name + "\"")
+        print("ecco i primi tre sinonimi secondo PubChem: " + str(composto.synonyms[:3]))
+        print(f"il CID del composto è {composto.cid}")
+        print("il peso molecolare del composto è di " + composto.molecular_weight + " g/mol")
+        print("il composto ha carica " + str(composto.charge))
+        print("---")
+    
+    elif tipo in tipo_3:
+        composto = pcp.get_compounds(input("inserisci nome: "), "name")[0]
+        print("---")
+        print("ecco i primi tre sinonimi secondo PubChem: " + str(composto.synonyms[:3]))
+        print(f"il CID del composto è {composto.cid}")
+        print("la formula del composto è " + composto.molecular_formula)
+        print("il peso molecolare del composto è di " + composto.molecular_weight + " g/mol")
+        print("il composto ha carica " + str(composto.charge))
+        print("---")
+
+    else:
+        print(f"\"{tipo}\" non è un valore adeguato, riprova.")
+        main()
 
 if __name__ == "__main__":
     main()
---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    import pH, info
+
+    scelta = input("1 - informazioni di un composto\n2 - calcoli del pH\ncosa vuoi fare? ")
+    print("---")
+    scelta = scelta.lower()
+    scelta_1 = ["1", "informazioni", "informazioni di un composto"]
+    scelta_2 = ["2", "ph", "calcoli del ph"]
+    if scelta in scelta_1:
+        info.main()
+    elif scelta in scelta_2:
+        pH.main()
+
+if __name__ == "__main__":
+    main()
